67fa194ef9896cd05d35cc73f375f2e95a860a72
Joe Brew <joebrew@gmail.com>
2014-09-11 11:57:20 -0400

    started point density mapping in assignment 2 of spatial epi

--- phc6194/lecture2/lecture2.R
+++ phc6194/lecture2/lecture2.R
@@ -10,29 +10,176 @@ library(Hmisc)
 ####################
 setwd("C:/Users/BrewJR/Documents/uf/phc6194/lecture2")
 
+#########
+# REMOVE BORDERS
+#########
+par(mar=c(0,0,0,0))
+par(oma=c(0,0,0,0))
+
+#########
+# ATTACH PACKAGES
+#########
+library(RColorBrewer)
+library(classInt)
+library(maps)
+library(rgdal)
+
+##########
+# READ IN WORLD MAP SHAPEFILE
+##########
+setwd("C:/Users/BrewJR/Desktop/")
+world <- readOGR("world_borders",layer = "world_borders")
+
+##########
+# CREATE SOME RANDOM X (LONG) AND Y (LAT) POINTS 
+# AND BIND INTO DF
+##########
+x <- runif(1000000, min=-180, max=190)
+y <- runif(1000000, min = -86, max=84)
+myPoints <- data.frame(cbind(x,y))
+
+###########
+# MAKE A SPATIAL POINTS FILE
+###########
+coordinates(myPoints) <- ~x+y
+
+###########
+# ASSIGN PROJECTION STRING
+###########
+proj4string(myPoints) <- proj4string(world)
+
+###########
+# CREATE VECTOR OF WHICH POLYGON EACH POINT IS IN
+###########
+myPolygons <- over(myPoints, polygons(world))
+
+###########
+# SAMPLE POINTS FOR EACH POLYGON
+###########
+
+# Pick a country 
+country <- "United States"
+
+# factor by which to reduce / increase point density
+redinc <- 0.000001
+
+nPoints <- ifelse(ceiling(redinc*sum(world$POP_CNTRY[which(world$CNTRY_NAME == country)],
+                              na.rm = TRUE)) > 0,
+              ceiling(redinc*sum(world$POP_CNTRY[which(world$CNTRY_NAME == country)],
+                                 na.rm = TRUE)),
+              1)
+
+# Sample points from that country
+countryPoints <- sample(x = myPoints[which(myPolygons %in% which(world$CNTRY_NAME == country))],
+            size = nPoints,
+            replace = TRUE)
+
+# Plot those points
+plot(countryPoints, pch=16, col=adjustcolor("black", alpha.f=0.6) )
+
+# LOOP TO PLOT ALL POINTS
+plot(1,1, pch=16, cex=10000000, xlim=c(-180,190), ylim=c(-86,84))
+for (i in unique(world$CNTRY_NAME)){
+  
+  # Pick a country 
+  country <- i
+  
+  # factor by which to reduce / increase point density
+  redinc <- 0.000001
+  
+  nPoints <- ifelse(ceiling(redinc*sum(world$POP_CNTRY[which(world$CNTRY_NAME == country)],
+                                       na.rm = TRUE)) > 0,
+                    ceiling(redinc*sum(world$POP_CNTRY[which(world$CNTRY_NAME == country)],
+                                       na.rm = TRUE)),
+                    1)
+  
+  
+  # Plot those points
+  if(length(countryPoints) > 0){
+    points(countryPoints, pch=16, col=adjustcolor("white", alpha.f=0.6) )
+    }else{return(NULL)}
+  Sys.sleep()
+  
+  
+}
+
+
+###########
+#ASSIGN COLOR VECTOR
+###########
+mycols <- colorRampPalette(brewer.pal(9, "Spectral"))(length(world$CNTRY_NAME))
+
+mycols2 <- mycols[myPolygons]
+
+plot(1,1, pch=16, cex=10000000, xlim=c(-180,190), ylim=c(-86,84))
+plot(myPoints,
+     pch = 1:20,
+     col = adjustcolor(mycols2, alpha.f = 0.2),
+     cex = 0.01,
+     add = TRUE)
+
+############
+# CREATE FUNCTION FOR MAPPING POINT DENSITY
+############
+# use sample
+
+
+############
+# MAP BY VOWELS IN COUNTRY NAME
+############
+mymap <- map("world")
+
+# REMOVE EVERYTHING AFTER COLON (INCLUDING COLON):
+mymap$names <- sub("(.*?):.*", "\\1", mymap$names)
+
+# CREATE COLUMN FOR NUMBER OF LETTERS
+mymap$letters <- nchar(as.character(mymap$names))
+
+# FUNCTION FOR PLOTTING WORLD
+MapFun <- function(var, color, border="black", proj="aitoff", param=NA){
+  plotvar <- var
+  nclr <- 5
+  plotclr <- (brewer.pal(nclr, color))
+  #class <- classIntervals(plotvar, nclr, style = "quantile", dataPrecision=0) #use "equal" instead
+  class <- classIntervals(plotvar, nclr, style="equal")
+  colcode <- findColours(class, plotclr)
+  legcode <- paste0(gsub(",", " - ", gsub("[[]|[]]|[)]", "", names(attr(colcode, "table")))))
+  map("world", projection = proj, param = param, 
+      fill = TRUE,
+      border=border, col=colcode)
+  legend("bottomleft", # position
+         legend = legcode, #names(attr(colcode, "table")), 
+         fill = attr(colcode, "palette"), 
+         cex = 0.85, 
+         border=grey)
+}
+
+par(mar=c(0,0,0,0))
+MapFun(var =  mymap$letters,
+       color = "Greys",
+       proj = "bonne",
+       param = 70)
+
+
+
+
+
+
 #######
 # COLORS
 #######
 mycols <- colorRampPalette(brewer.pal(8, "Greens"))(nrow(blocks)) 
 mycols <- sample(mycols)
 
-########
-# READ FROM MDB
-########
-FLCounties <- readOGR("MapProjection.mdb",layer = "FLCounties")
-County_Boundary <- readOGR("MapProjection.mdb",layer = "County_Boundary")
-Alachua_BlockGroup <- readOGR("MapProjection.mdb",layer = "Alachua_BlockGroup")
-Country08 <- readOGR("MapProjection.mdb",layer = "Country08")
-World30 <- readOGR("MapProjection.mdb",layer = "World30")
-States <- readOGR("MapProjection.mdb",layer = "States")
-Hospitals_WrongPrj <- readOGR("MapProjection.mdb",layer = "Hospitals_WrongPrj")
 
 
-library(maps)
-par(mar=c(0,0,0,0))
-par(oma=c(0,0,0,0))
 
-library(RColorBrewer)
+
+
+
+
+
+
 mycols <- colorRampPalette(c("brown", "grey", "beige", "darkgrey"))(20)
 mycols <- adjustcolor(mycols, alpha.f=0.8)
 
@@ -62,14 +209,40 @@ map("world",
 
 
 
+
+########
+# READ FROM MDB
+########
+# FLCounties <- readOGR("MapProjection.mdb",layer = "FLCounties")
+# County_Boundary <- readOGR("MapProjection.mdb",layer = "County_Boundary")
+# Alachua_BlockGroup <- readOGR("MapProjection.mdb",layer = "Alachua_BlockGroup")
+# Country08 <- readOGR("MapProjection.mdb",layer = "Country08")
+# World30 <- readOGR("MapProjection.mdb",layer = "World30")
+# States <- readOGR("MapProjection.mdb",layer = "States")
+# Hospitals_WrongPrj <- readOGR("MapProjection.mdb",layer = "Hospitals_WrongPrj")
+# 
+
+# BASE COLOR OFF LETTERS
+mycols <- colorRampPalette(c("grey",  "white"))(max(mymap$letters))
+mycols <- adjustcolor(mycols, alpha.f=0.8)
+mymap$color <- mycols[mymap$letters]
+
+
+map("world", 
+    fill = TRUE,
+    col = "white",
+    border = "black",
+    bg = "beige")
+
+
 map("world", projection = "", param=1,
     fill = TRUE,
-    col = mycols,
+    col = mymap$color,
     border = bor, 
     bg = "black")
 
 map("world", 
     fill = TRUE,
-    col = mycols,
+    col = mymap$color,
     border = "black",
     bg = "black")
